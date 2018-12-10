8e3cc0c546154fd7bf41f818b65cb33fad5b3a93
Paul <paul.ayling@bglgroup.co.uk>
2018-12-10 16:04:21 +0000

    Added Cohort Input

--- directory.rb
+++ directory.rb
@@ -1,21 +1,31 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
   # create an empty array
   students = []
+  puts "Please enter Student Information"
+  puts "To finish, just hit return twice"
   # get the first name
+  puts "Please enter Name"
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student has to the array
-    students << {name: name, cohort: :november, hobbies: :football, height: :height, country: :country}
+    nameandco = [] << name
+    # prompt for cohort, default to November if empty
+    puts "Please enter Cohort, default is November"
+    nameandco << cohort = gets.chomp
+    if nameandco[1] == ""
+      nameandco[1] = "November"
+    end
+    # add information to students array
+    students << {name: nameandco[0], cohort: nameandco[1], hobbies: :football, height: :height, country: :country}
     puts "Now we have #{students.count} students"
     # get another name from the suer
+    puts "Please enter another name"
     name = gets.chomp
-    # return the array of input_students
   end
+  # return the array of input_students
     return students
 end
+
 
 def print_header
   puts "The students of Villains Academy"
