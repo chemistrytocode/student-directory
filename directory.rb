ad750b89914305c3b93bb68ad3820785a8945ef1
Paul <paul.ayling@bglgroup.co.uk>
2018-12-10 11:56:15 +0000

    Added Input Prompt

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-# let's put all students into an array
-students = [
-    {name: "Dr. Hannibal Lector", cohort: "November"},
-    {name: "Darth Vadar", cohort: "November"},
-    {name: "Nurse Ratched", cohort: "November"},
-    {name: "Michael Corleone", cohort: "November"},
-    {name: "Alex DeLarge", cohort: "November"},
-    {name: "The Wicked Witch of the West", cohort: "November"},
-    {name: "Terminator", cohort: "November"},
-    {name: "Freddy Krueger", cohort: "November"},
-    {name: "The Joker", cohort: "November"},
-    {name: "Joffrey Baratheon", cohort: "November"},
-    {name: "Norman Bates", cohort: "November"},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the suer
+    name = gets.chomp
+    # return the array of input_students
+  end
+    return students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +32,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
