29253d180d8b4b69beb7791a2b7a13f9e85ca4c5
Paul <paul.ayling@bglgroup.co.uk>
2018-12-10 16:23:28 +0000

    Refactered to Map and added Cohort Group Method

--- directory.rb
+++ directory.rb
@@ -8,15 +8,15 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    nameandco = [] << name
     # prompt for cohort, default to November if empty
     puts "Please enter Cohort, default is November"
-    nameandco << cohort = gets.chomp
-    if nameandco[1] == ""
-      nameandco[1] = "November"
+    cohort = gets.chomp
+    nameandco = {:name => name, :cohort => cohort}
+    if nameandco[:cohort] == ""
+      nameandco[:cohort] = "November"
     end
     # add information to students array
-    students << {name: nameandco[0], cohort: nameandco[1], hobbies: :football, height: :height, country: :country}
+    students << {name: nameandco[:name], cohort: nameandco[:cohort], hobbies: :football, height: :height, country: :country}
     puts "Now we have #{students.count} students"
     # get another name from the suer
     puts "Please enter another name"
@@ -35,7 +35,7 @@ end
 def print(students)
   students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]}".center(50)
-    puts "Cohort:#{student[:cohort]}".center(50)
+    puts "Cohort: #{student[:cohort]}".center(50)
     puts "Height: #{:height}".center(50)
     puts "Country: #{:country}".center(50)
     puts "Hobbies: #{:hobbies}\n".center(50)
@@ -45,8 +45,20 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
+def student_cohorts(students)
+  puts "Enter a Cohort Date"
+  input = gets.chomp
+  students.each do |student, index|
+    if student[:cohort] == input
+      puts student[:name]
+    end
+  end
+end
+
 #nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
 print_footer(students)
+student_cohorts(students)
