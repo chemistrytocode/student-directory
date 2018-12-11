522aaf97ff45298047cbff2c1821bd04ea3bc9bc
Paul <paul.ayling@bglgroup.co.uk>
2018-12-11 15:58:06 +0000

    Added load default if no ARGV

--- directory.rb
+++ directory.rb
@@ -80,9 +80,10 @@ end
 
 def try_load_students
   filename = ARGV.first# first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
+  if filename.nil? # get out of the method if it isn't given
+    load_students
+  elsif File.exists?(filename) # if it exists
+    load_students
      puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
