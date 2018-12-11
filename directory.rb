de89e708135a00b51d685cf83b3129db5395df9d
Paul <paul.ayling@bglgroup.co.uk>
2018-12-11 11:22:33 +0000

    Added Saving to CSV

--- directory.rb
+++ directory.rb
@@ -7,24 +7,41 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
 def process(selection)
   case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
     puts "I don't know what you meant, try again"
   end
 end
 
-
-def print_menu
-  puts "1. Input the Students"
-  puts "2. Show the Students"
-  puts "9. Exit"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
 end
 
 def show_students
@@ -33,36 +50,17 @@ def show_students
   print_footer
 end
 
-def input_students
-  # create an empty array
-  puts "Please enter Student Information"
-  puts "To finish, just hit return twice"
-  # get the first name
-  puts "Please enter Name"
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # prompt for cohort, default to November if empty
-    puts "Please enter Cohort, default is November"
-    cohort = gets.chomp
-    nameandco = {:name => name, :cohort => cohort}
-    if nameandco[:cohort] == ""
-      nameandco[:cohort] = "November"
-    end
-    # add information to students array
-    @students << {name: nameandco[:name], cohort: nameandco[:cohort]}
-    # check for plurals
-    if @students.count == 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    # get another name from the suer
-    puts "Please enter another name"
-    name = gets.chomp
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
   end
+  file.close
 end
-
 
 def print_header
   puts "The students of Villains Academy"
@@ -70,30 +68,13 @@ def print_header
 end
 
 def print_student_list
-  @students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]}".center(50)
-    puts "Cohort: #{student[:cohort]}".center(50)
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} great student"
-  else
-    puts "Overall, we have #{@students.count} great students"
-  end
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def student_cohorts
-  puts "Enter a Cohort Date to search"
-  input = gets.chomp
-  @students.each do |student, index|
-    if student[:cohort] == input
-      puts student[:name]
-    end
-  end
-end
-
-
-#nothing happens until we call the methods
 interactive_menu
