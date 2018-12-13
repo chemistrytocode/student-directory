a28a897788cad437e7bf76a0371a1bb630595586
Paul <paul.ayling@bglgroup.co.uk>
2018-12-13 13:28:34 +0000

    Added Custom Save Feature and Refactored

--- directory.rb
+++ directory.rb
@@ -10,8 +10,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a location"
+  puts "4. Load the list from a location"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -24,7 +24,7 @@ def process(selection)
   when "3"
     save_students
   when "4"
-    load_prompt
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -58,14 +58,29 @@ def show_students
   print_footer
 end
 
+# SAVING METHODS START
+# Prompt the user for save location
+def save_prompt
+  puts "Enter a save location for this file"
+  puts "Leave empty to save to default"
+  filename = gets.chomp
+    if filename == "" || nil
+      filename = "students.csv"
+    else
+      filename
+    end
+end
+
+# Save the file
 def save_students
+  filename = save_prompt
   # open the file for writing
-  File.open("students.csv", "w") do
+  File.open(filename, "w") do |student_list|
     # iterate over the array of students
     @students.each do |student|
       student_data = [student[:name], student[:cohort]]
       csv_line = student_data.join(",")
-      file.puts csv_line
+      student_list.puts csv_line
       end
     end
 end
@@ -78,27 +93,30 @@ def load_prompt
   if filename == ""
     filename = "students.csv"
   end
-  check_load_file(filename)
+  return filename
 end
 
 # Checks if argument file is valid
 def check_load_file(filename)
-    if File.exists?(filename) # if it exists
-      load_students(filename)
-    else # if it doesn't exist
+    puts filename
+    if File.exists?(filename) # if file doesn't exist
+      puts "Loading from #{filename}..."
+    else
       puts "Sorry, #{filename} doesn't exist."
       interactive_menu
     end
 end
 
 # Loads students
-def load_students(filename)
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-  add_to_hash(name, cohort.to_sym)
+def load_students
+  filename = load_prompt
+  check_load_file(filename)
+  File.open(filename, "r") do |student_list|
+    student_list.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    add_to_hash(name, cohort.to_sym)
+    end
   end
-  file.close
   puts "Loaded #{@students.count} from #{filename}"
 end
 
